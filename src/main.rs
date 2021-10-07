5213c68ec3da4692995d9c6650aaf1d90e45e948
kriscirx <kriscirx@gmail.com>
2021-10-08 04:05:25 +0700

    Update IP Address

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 use actix_web::{web, App, HttpServer};
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/hello", web::get().to(cargo_sample::manual_hello))
-    })
-    .bind("0.0.0.0:8000")?
-    .run()
-    .await
+  dotenv::dotenv().ok();
+  
+  let ip = env::var("IP")
+    .unwrap_or_else(|_| "0.0.0.0".to_string());
+
+  let port = env::var("PORT")
+    .unwrap_or_else(|_| "3000".to_string())
+    .parse()
+    .expect("PORT must be a number");
+
+  HttpServer::new(|| {
+    App::new()
+      .route("/hello", web::get().to(cargo_sample::manual_hello))
+  })
+  .bind((ip, port))?
+  .run()
+  .await
 }
